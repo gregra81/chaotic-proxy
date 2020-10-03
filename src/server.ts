cce9d674f5e42f56af5713f18e60d8eef01d4b2b
Greg Rashkevitch <rashkevitch@gmail.com>
2020-10-03 14:04:26 +0300

    use morgan logger

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import * as express from 'express';
+import { Request, Response } from 'express';
+import * as morgan from 'morgan';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 import * as ChaoticResponse from 'connect-chaotic-response';
 import config from './config';
@@ -9,27 +11,26 @@ import config from './config';
 const app = express();
 app.disable('x-powered-by');
 
-const chaoticResponse = new ChaoticResponse({ chaotic: { mode: config.proxy.chaosMode } });
+const chaoticResponse = new ChaoticResponse( { mode: config.proxy.chaosMode as ChaoticResponse.Modes });
 
 const proxyOptions = {
   target: config.proxy.target,
   changeOrigin: true,
   pathRewrite: {
     '^/proxy/': '/', // remove base path
-  },
-  onProxyReq: (_proxyReq, req: express.Request) => {
-    const log = {
-      url: req.url,
-      method: req.method,
-      headers: req.headers,
-      body: req.body,
-      query: req.query,
-    };
-    console.log(log);
-  },
+  }
 };
 
-app.use(chaoticResponse.middleware); // chaotic middleware should be first!
+// chaotic middleware - this should be first!
+app.use(chaoticResponse.middleware);
+console.log(`Chaotic behaviour is set: Chaotic mode is ${config.proxy.chaosMode}`);
+
+// logger
+morgan.token('host', function(req: Request) {
+  return req.hostname;
+});
+
+app.use(morgan('[:date[clf]] ":host :method :url HTTP/:http-version" :status :res[content-length] - :response-time ms'));
 
 // Proxy
 app.use('/proxy', createProxyMiddleware(proxyOptions));
